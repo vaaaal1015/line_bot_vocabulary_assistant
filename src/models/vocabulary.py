97141c425c81ae1363eecae540c8180ec253cac9
Tony Su <susu2721404@gmail.com>
2024-06-24 23:14:26 +0800

    build(vocabulary): add to str

--- src/models/vocabulary.py
+++ src/models/vocabulary.py
@@ -3,10 +3,13 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 
 class Vocabulary(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    english: Mapped[str] = mapped_column(unique=True)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    english: Mapped[str] = mapped_column(unique=True, nullable=False)
     chinese: Mapped[str] = mapped_column(nullable=False)
 
     def __init__(self, english: str, chinese: str):
         self.english = english
         self.chinese = chinese
+
+    def __str__(self) -> str:
+        return f"{self.english} {self.chinese}"
