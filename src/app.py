a954a2021619814ad6ee283d51353414e4d9d910
Tony Su <susu2721404@gmail.com>
2024-06-29 22:15:23 +0800

    feat: exam return FlexMessage

--- src/app.py
+++ src/app.py
@@ -8,7 +8,13 @@ from linebot.v3.messaging import (
     MessagingApi,
     ReplyMessageRequest,
     TextMessage,
-)
+    #   ButtonsTemplate, TemplateMessage,
+    MessageAction,
+    FlexMessage,
+    FlexBubble,
+    FlexBox,
+    FlexText,
+    FlexButton)
 from linebot.v3.webhooks import MessageEvent, TextMessageContent
 from .shared.db import db
 from .models.vocabulary import Vocabulary
@@ -27,6 +33,82 @@ config = dotenv_values(dotenv_path="../.env")
 
 configuration = Configuration(access_token=config["CHANNEL_ACCESS_TOKEN"])
 handler = WebhookHandler(config["CHANNEL_SECRET"])
+
+
+def add_vocabulary(english: str, chinese: str) -> TextMessage:
+    app.logger.debug(f"enginsh:{english}")
+    app.logger.debug(f"chinese:{chinese}")
+    exists = Vocabulary.query.filter_by(english=english).first()
+    if exists:
+        return TextMessage(text="新增失敗，單字已存在")
+    vocabulary = Vocabulary(english=english, chinese=chinese)
+    db.session.add(vocabulary)
+    db.session.commit()
+    return TextMessage(text="新增成功")
+
+
+def list_vocabulary(page: int) -> TextMessage:
+    if page == 0:
+        count = Vocabulary.query.count()
+        return TextMessage(text=f"總頁數:{ceil(count / 10)}")
+    obj = Vocabulary.query.order_by(Vocabulary.id.asc())
+    page_objs = obj.paginate(
+        page=page,
+        per_page=10,
+    ).items
+    string = f"第{page}頁\n"
+    string = string + "\n".join(map(str, page_objs))
+    return TextMessage(text=string)
+
+
+def remove_vocabulary(english: str) -> TextMessage:
+    count = Vocabulary.query.filter_by(english=english).delete()
+    db.session.commit()
+    if count == 0:
+        return TextMessage(text="刪除失敗，沒有該單字")
+    return TextMessage(text="刪除成功")
+
+
+def exam_vocabulary() -> FlexMessage:
+    question = Vocabulary.query.order_by(Vocabulary.point,
+                                         func.random()).first()
+    selection = Vocabulary.query.filter(Vocabulary.id != question.id).order_by(
+        func.random()).limit(4).all()
+    selection.append(question)
+    buttons = [
+        FlexButton(action=MessageAction(
+            label=item.chinese,
+            text=f":ans {question.english} {item.chinese}"))
+        for item in selection
+    ]
+    random.shuffle(buttons)
+    return FlexMessage(altText="exam",
+                       contents=FlexBubble(
+                           size="micro",
+                           header=FlexBox(
+                               layout="vertical",
+                               alignItems="center",
+                               contents=[FlexText(text=question.english)]),
+                           body=FlexBox(layout="vertical", contents=buttons)))
+
+
+def answer_vocabulary(question: str, answer: str) -> TextMessage:
+    try:
+        vocabulary = Vocabulary.query.filter_by(english=question).one()
+        if vocabulary.chinese == answer:
+            vocabulary.bonus_point()
+            reply = "回答正確"
+        else:
+            vocabulary.deduct_point()
+            reply = f"回答錯誤，{str(vocabulary)}"
+        Vocabulary.query.filter_by(english=question).update(
+            {Vocabulary.point: vocabulary.point})
+        db.session.commit()
+        return TextMessage(text=reply)
+    except NoResultFound:
+        return TextMessage(text="回答失敗，沒有該單字")
+    except MultipleResultsFound:
+        return TextMessage(text="錯誤")
 
 
 @app.route("/callback", methods=["POST"])
@@ -49,96 +131,31 @@ def callback():
     return "OK"
 
 
-def add_vocabulary(english: str, chinese: str) -> str:
-    app.logger.debug(f"enginsh:{english}")
-    app.logger.debug(f"chinese:{chinese}")
-    exists = Vocabulary.query.filter_by(english=english).first()
-    if exists:
-        return "新增失敗，單字已存在"
-    vocabulary = Vocabulary(english=english, chinese=chinese)
-    db.session.add(vocabulary)
-    db.session.commit()
-    return "新增成功"
-
-
-def list_vocabulary(page: int) -> str:
-    if page == 0:
-        count = Vocabulary.query.count()
-        return f"總頁數:{ceil(count / 10)}"
-    obj = Vocabulary.query.order_by(Vocabulary.id.asc())
-    page_objs = obj.paginate(
-        page=page,
-        per_page=10,
-    ).items
-    string = f"第{page}頁\n"
-    string = string + "\n".join(map(str, page_objs))
-    return string
-
-
-def remove_vocabulary(english: str) -> str:
-    count = Vocabulary.query.filter_by(english=english).delete()
-    db.session.commit()
-    if count == 0:
-        return "刪除失敗，沒有該單字"
-    return "刪除成功"
-
-
-def exam_vocabulary() -> str:
-    question = Vocabulary.query.order_by(Vocabulary.point,
-                                         func.random()).first()
-    selection = Vocabulary.query.filter(Vocabulary.id != question.id).order_by(
-        func.random()).limit(4).all()
-    chinese_selection = [item.chinese for item in selection]
-    chinese_selection.append(question.chinese)
-    random.shuffle(chinese_selection)
-    string = f"{question.english}\n----------\n"
-    string = string + "\n".join(chinese_selection)
-    return string
-
-
-def answer_vocabulary(question: str, answer: str) -> str:
-    try:
-        vocabulary = Vocabulary.query.filter_by(english=question).one()
-        if vocabulary.chinese == answer:
-            vocabulary.bonus_point()
-            reply = "回答正確"
-        else:
-            vocabulary.deduct_point()
-            reply = f"回答錯誤，{str(vocabulary)}"
-        Vocabulary.query.filter_by(english=question).update(
-            {Vocabulary.point: vocabulary.point})
-        db.session.commit()
-        return reply
-    except NoResultFound:
-        return "回答失敗，沒有該單字"
-    except MultipleResultsFound:
-        return "錯誤"
-
-
 @handler.add(MessageEvent, message=TextMessageContent)
 def handle_message(event):
     with ApiClient(configuration) as api_client:
         line_bot_api = MessagingApi(api_client)
         msg = event.message.text.lower().split()
-        reply = "失敗，沒有符合的指令"
 
         if (msg[0] == ":add" and len(msg) == 3):
-            reply = add_vocabulary(english=msg[1], chinese=msg[2])
+            messages = add_vocabulary(english=msg[1], chinese=msg[2])
         elif (msg[0] == ":ls" and len(msg) == 2):
-            reply = list_vocabulary(page=int(msg[1]))
+            messages = list_vocabulary(page=int(msg[1]))
         elif (msg[0] == ":ls" and len(msg) == 1):
-            reply = list_vocabulary(page=0)
+            messages = list_vocabulary(page=0)
         elif (msg[0] == ":rm" and len(msg) == 2):
-            reply = remove_vocabulary(msg[1])
+            messages = remove_vocabulary(msg[1])
         elif (msg[0] == ":exam"):
-            reply = exam_vocabulary()
+            messages = exam_vocabulary()
         elif (msg[0] == ":ans" and len(msg) == 3):
-            reply = answer_vocabulary(question=msg[1], answer=msg[2])
+            messages = answer_vocabulary(question=msg[1], answer=msg[2])
+        else:
+            messages = TextMessage(text="失敗，沒有符合的指令")
 
         line_bot_api.reply_message_with_http_info(
             ReplyMessageRequest(
                 reply_token=event.reply_token,
-                messages=[TextMessage(text=reply)],
+                messages=[messages],
             ))
 
 
--- src/models/vocabulary.py
+++ src/models/vocabulary.py
@@ -6,12 +6,7 @@ class Vocabulary(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     english: Mapped[str] = mapped_column(unique=True, nullable=False)
     chinese: Mapped[str] = mapped_column(nullable=False)
-    point: Mapped[int] = mapped_column(nullable=False, default=5)
-
-    def __init__(self, english: str, chinese: str) -> None:
-        self.english = english
-        self.chinese = chinese
-        self.point = 0
+    point: Mapped[int] = mapped_column(nullable=False, default=0)
 
     def __str__(self) -> str:
         return f"{self.english} {self.chinese}"
